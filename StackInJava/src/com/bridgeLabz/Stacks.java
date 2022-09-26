c2c67e8a8c1feeed65d691dbf927d795c6e0dc27
Bharath-SR <bharath.s0941@gmail.com>
2022-09-26 07:28:43 +0530

    Started Stacks And Queues

---
+++ StackInJava/src/com/bridgeLabz/Stacks.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+/**
+ * @author Admin
+ *
+ */
+public class Stacks {
+
+	/**
+	 * Stared stack
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
