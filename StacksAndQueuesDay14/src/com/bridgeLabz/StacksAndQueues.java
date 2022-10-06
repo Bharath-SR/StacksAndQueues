69bc1aeadb1861451dcd5623accec403b2ea2b64
Bharath-SR <bharath.s0941@gmail.com>
2022-10-06 16:37:23 +0530

    UC2:Performed Pop Operation

--- StacksAndQueuesDay14/src/com/bridgeLabz/StacksAndQueues.java
+++ StacksAndQueuesDay14/src/com/bridgeLabz/StacksAndQueues.java
@@ -12,19 +12,20 @@ import java.util.Stack;
 public class StacksAndQueues {
 
 	/**
-	 * UC1-Started stacks
+	 * UC2-Perform Peak and Pop
 	 */
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Stack stack = new Stack();
-		System.out.println("Stcak Elements are: ");
 		stack.push(70);
 		System.out.println(stack);
 		stack.push(30);
 		System.out.println(stack);
 		stack.push(56);
+		System.out.println(stack);
+		stack.pop();
 		System.out.println(stack);
 	
 		
