4428ed8ed623466f4a484920b4ab127173f8f19d
Bharath-SR <bharath.s0941@gmail.com>
2022-10-06 16:31:00 +0530

    UC1:Started Stack

---
+++ StacksAndQueuesDay14/src/com/bridgeLabz/StacksAndQueues.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+import java.util.Stack;
+
+/**
+ * @author Admin
+ *
+ */
+public class StacksAndQueues {
+
+	/**
+	 * UC1-Started stacks
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack stack = new Stack();
+		System.out.println("Stcak Elements are: ");
+		stack.push(70);
+		System.out.println(stack);
+		stack.push(30);
+		System.out.println(stack);
+		stack.push(56);
+		System.out.println(stack);
+	
+		
+	}
+	}
+
+
